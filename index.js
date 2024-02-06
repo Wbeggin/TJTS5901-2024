abf530959a7820c8ca498de650c0783efe638b18
Weerti Montonen <weerti.p.montonen@student.jyu.fi>
2024-02-06 17:31:09 +0200

    added get to /api

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World from index.js!")
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { ok } = require('assert')
+
+app.get('/api/', (request, response) => {
+    response.json(ok)
+  })
+
+  const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
