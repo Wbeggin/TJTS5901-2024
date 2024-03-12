c3f25db6a0d6b142deed9dd48d6a6306180cb414
Weerti Montonen <weerti.p.montonen@student.jyu.fi>
2024-03-12 18:36:53 +0200

    refactored code and added mongodb connection

--- src/Models/Order.js
+++ src/Models/Order.js
@@ -1,26 +1,36 @@
-class Order {
-    constructor(userId, price, type, quantity, lastTradedPrice) {
-        if (!userId || !price || !type || !quantity || !lastTradedPrice) {
-            throw new Error('Missing necessary arguments to create an Order.');
-            }
-        if (parseFloat(price.toFixed(2)) !== price) {
-            throw new Error('Price must be up to two decimal places.');
-            }
-        if (type !== 'BID' && type !== 'OFFER') {
-            throw new Error('Type must be either BID    or OFFER.');
-        }
-        if (!Number.isInteger(quantity)) {
-            throw new Error('Quantity must be an integer.');
-        }
-        if (price < lastTradedPrice * 0.9 || price > lastTradedPrice * 1.1) {
-            throw new Error('Price must be within 10% of the last traded price.');
-        }
-        this.quantity = quantity;
-        this.userId = userId;
-        this.price = price;
-        this.type = type; // "BID" or "OFFER"
-        this.lastTradedPrice = lastTradedPrice;
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ['BID', 'OFFER'],
+    message: 'Type must be either BID or OFFER.'
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: Number.isInteger,
+      message: 'Quantity must be an integer.'
     }
   }
+})
 
-module.exports = Order;
+  orderSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
+module.exports = mongoose.model('Order', orderSchema);
