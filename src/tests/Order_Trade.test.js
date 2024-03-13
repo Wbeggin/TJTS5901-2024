c91222045ca4aebe5b98a6ac3cba54ee919c6c76
Weerti Montonen <weerti.p.montonen@student.jyu.fi>
2024-03-13 14:03:07 +0200

    added tests

--- src/tests/Order_Trade.test.js
+++ src/tests/Order_Trade.test.js
@@ -1,11 +1,29 @@
-const { test, describe, expect } = require('@jest/globals');
-const { createBid, createOffer} = require('../Controllers/OrderController');
-const Order = require('../Models/Order');
-const OrderController = require('../Controllers/OrderController');
-
+const { test, describe, expect } = require('@jest/globals')
+const { createBid, createOffer} = require('../Controllers/OrderController')
+const Order = require('../Models/Order')
+const OrderController = require('../Controllers/OrderController')
+const Trade = require('../Models/Trade')
 const supertest = require('supertest')
-const app = require('../index');
+const app = require('../index')
 const api = supertest(app)
+
+describe('GET /api/AAPL/trades', () => {
+  test('should return all trades', async () => {
+      const response = await api.get('/api/AAPL/trades')
+      expect(response.statusCode).toBe(200)
+      expect(response.body).toBeInstanceOf(Array)
+  })
+})
+
+describe('GET /api/AAPL/stock', () => {
+  test('should return stock data', async () => {
+    const response = await api.get('/api/AAPL/stock')
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toBeInstanceOf(Object)
+  })
+})
+
+
 
 describe('POST /api/AAPL/order', () => {
   test('should create a new offer and return 200 status', async () => {
@@ -16,6 +34,7 @@ describe('POST /api/AAPL/order', () => {
       quantity: 1
     }
     await api.post('/api/AAPL/order').send(newOffer).expect(200).expect('Content-Type', /application\/json/)
+    await Order.deleteOne({userId: 'user1', price: 170.00, type: 'OFFER', quantity: 1})
   })
 
 
@@ -58,3 +77,17 @@ describe('Order', () => {
   });
 
 });
+
+describe('Trade', () => {
+  test('should correctly instantiate with provided arguments', () => {
+    const trade = new Trade({time: new Date(), price: 150, quantity: 10})
+    expect(trade.time).toBeInstanceOf(Date)
+    expect(trade.price).toBe(150.00)
+    expect(trade.quantity).toBe(10)
+  })
+
+  test('should throw an error when instantiating with missing arguments', async () => {
+    const trade = new Trade({time: new Date(), quantity: 10})
+    await expect(trade.save()).rejects.toThrow('Trade validation failed: price: Path `price` is required.')
+  })
+})
